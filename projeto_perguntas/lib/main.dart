486b7926dd9ad5e9ec854e7b632c6efecf6ef947
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-03-08 23:42:33 -0300

    Criando o primeiro Widget nesse app

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
 
+main() {
+  runApp(new PerguntaApp());
+}
+
+// vamos criar um widget
+class PerguntaApp extends StatelessWidget {
+  /*
+  -> Esse StatelessWidget é que faz a classe PerguntaApp se tornar um Widget
+  */
+
+  // metodo que vai criar o widget
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Olá Flutter!!!'),
+    );
+  }
+}
